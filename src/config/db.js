4017953dfcfaa2b6bc003ea5174b4535438660c0
Anukriti-Nawani <anunwn@gmail.com>
2023-01-24 12:47:18 +0530

    files

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const DB_URL =
 	process.env.DB_URL ||
-	"mongodb+srv://A:A@cluster0.81swo.mongodb.net/growcalculator";
+	"mongodb+srv://A:A@cluster0.81swo.mongodb.net/jobposting";
 const connect = async () => {
 	console.log();
 	clientserver = await mongoose.connect(DB_URL);
