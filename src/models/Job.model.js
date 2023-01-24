148e760273b2b82e2eb8c9afe79ec2c572bd8f60
Anukriti-Nawani <anunwn@gmail.com>
2023-01-24 14:19:16 +0530

    files

--- src/models/Job.model.js
+++ src/models/Job.model.js
@@ -1,16 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const jobSchema = mongoose.Schema({
-    company: { type: String },
-    city: { type: String },
-    location: { type: String },
-    role: { type: String },
-    level: { type: String },
-    position: { type: String },
-    language: { type: String },
-    contract: { type: String },
-    postedAt:{ type: String }
-})
+  company: { type: String },
+  city: { type: String },
+  location: { type: String },
+  role: { type: String },
 
-const JobModel = mongoose.model('joblists', jobSchema);
+  language: { type: String },
+  contract: { type: String },
+});
+
+const JobModel = mongoose.model("joblists", jobSchema);
 
 module.exports = JobModel;
